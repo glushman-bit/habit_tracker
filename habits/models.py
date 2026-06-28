8b3a5a806655bed030d784df153e2bbbc64c1e29
Ivan Glushenkov <glushman966@gmail.com>
2026-06-28 09:40:14 +0300

    setting models.py for habits and users, create migrations

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Habit(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Создатель привычки'
+    )
+    place = models.CharField(
+        max_length=100,
+        verbose_name='Место выполнения привычки'
+    )
+    time = models.TimeField(
+        null=True,
+        blank=True,
+        verbose_name='Время выполнения привычки'
+    )
+    action = models.CharField(
+        max_length=100,
+        verbose_name='Действие'
+    )
+    is_pleasant = models.BooleanField(
+        default=False,
+        verbose_name='Признак приятной привычки'
+    )
+    related_habit = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Связанная привычка'
+    )
+    periodicity = models.PositiveIntegerField(
+        default=1,
+        verbose_name='Периодичность привычки (раз в день)',
+    )
+    reward = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name='Вознаграждение',
+    )
+    duration = models.IntegerField(
+        verbose_name='Время выполнения привычки (в секундах)'
+    )
+    is_public = models.BooleanField(
+        default=False,
+        verbose_name='Признак публичности'
+    )
+
+    def __str__(self):
+        return f'{self.user}: в {self.time} выполняет {self.action} в {self.place}'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django_countries.fields import CountryField
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Email пользователя',
+        help_text='Укажите Email',
+    )
+    phone_number = PhoneNumberField(
+        unique=True,
+        blank=True,
+        null=True,
+        verbose_name='Phone Number',
+        help_text='Укажите номер телефона'
+    )
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        null=True,
+        blank=True,
+        default='avatars/default.jpg',
+        verbose_name='Загрузите аватар'
+    )
+    country = CountryField(
+        blank_label="Country",
+        help_text="Выберите страну",
+        blank=True,
+        null=True,
+    )
+    tg_nickname = models.CharField(
+        max_length=55,
+        blank=True,
+        null=True,
+        verbose_name="Ник в телеграмме",
+        help_text="Укажите ник в ТГ"
+    )
+    tg_chat_id = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name="Телеграмм chat-id",
+        help_text="Укажите chat-id в телеграмм"
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now,
+        verbose_name="Дата создания",
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
