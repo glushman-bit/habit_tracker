0afed1e80d75f668b1f259815f8e3aa791d2d4df
Ivan Glushenkov <glushman966@gmail.com>
2026-06-28 15:00:56 +0300

    create command createsuperuser - 'csu'

---
+++ users/management/commands/csu.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    """Команда для создания суперпользователя."""
+
+    help = "Создание суперпользователя"
+
+    def handle(self, *args, **options):
+        user = User.objects.create(email="admin@sky.pro")
+        user.set_password("1Q2w3e4r")
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        self.stdout.write(self.style.SUCCESS(f"Пользователь <{user.email}> успешно создан."))
+        user.save()
