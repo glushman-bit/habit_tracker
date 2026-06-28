718ee6cd2ab3ad37335ff31cf4a61b75569b7507
Ivan Glushenkov <glushman966@gmail.com>
2026-06-28 14:41:59 +0300

    create UserViewSet, UserCreateAPIView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from .serializaers import UserSerializer, UserCreateSerializer
+from .models import User
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """Класс для работы с пользователями."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    """Класс создания пользователя."""
+
+    serializer_class = UserCreateSerializer
+    permission_classes = [AllowAny]
