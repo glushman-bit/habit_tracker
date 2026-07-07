5e8ff414b4824730b47501fb9df9e40d087e5847
Ivan Glushenkov <glushman966@gmail.com>
2026-07-07 20:41:58 +0300

    update views.py add permissions

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-from .serializers import UserSerializer, UserCreateSerializer
+from .permissions import IsProfile
+from .serializers import UserSerializer, UserCreateSerializer, UserViewSerializer
 from .models import User
 
 
@@ -9,8 +10,30 @@ class UserViewSet(viewsets.ModelViewSet):
     """Класс для работы с пользователями."""
 
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    # serializer_class = UserViewSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        """Права доступа для изменения профиля пользователя."""
+
+        if self.action in ['update', 'partial_update']:
+            self.permission_classes = [IsProfile]
+
+        return super().get_permissions()
+
+    def get_serializer_class(self):
+        """Переопределение сериалайзера для просмотра профиля пользователя."""
+
+        if self.action == 'retrieve':
+            user = self.get_object()
+
+            if user != self.request.user:
+                return UserViewSerializer
+
+        elif self.action == 'list':
+            return UserViewSerializer
+
+        return UserSerializer
 
 
 class UserCreateAPIView(generics.CreateAPIView):
