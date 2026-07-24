16cbf48c765fd076993caea2b6fdc3aa068ff9a8
Ivan Glushenkov <glushman966@gmail.com>
2026-07-24 19:44:05 +0300

    update README.md, update requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -187,14 +187,3 @@ if "test" in sys.argv:
             "NAME": BASE_DIR / "test.sqlite3",
         }
     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql",
-#             'NAME': os.getenv('POSTGRES_DB'),
-#             'USER': os.getenv('POSTGRES_USER'),
-#             'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-#             'HOST': os.getenv('POSTGRES_HOST'),
-#             'PORT': os.getenv('POSTGRES_PORT'),
-#         }
-#     }
